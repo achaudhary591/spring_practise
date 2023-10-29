cd557fac2e34148dfe057141ca378fd5d6686cf7
achaudhary591 <achaudhary591@gmail.com>
2023-10-29 14:44:58 +0530

    added user and returning object

--- src/main/java/com/akshay/springbootpractise/controller/HomeController.java
+++ src/main/java/com/akshay/springbootpractise/controller/HomeController.java
@@ -1,22 +1,78 @@
 package com.akshay.springbootpractise.controller;
 
 import com.akshay.springbootpractise.models.User;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 @RestController
 public class HomeController {
+
+    private final List<User> users = new ArrayList<>();
+
+    Random random = new Random();
+
+    // Constructor to populate sample users
+    public HomeController() {
+        for (int i = 1; i <= 20; i++) {
+            User user = new User();
+            user.setId(i);
+            user.setName("User" + i);
+            user.setEmail("user" + i + "@example.com");
+            users.add(user);
+        }
+    }
 
     @RequestMapping("/")
     public String home(){
         return "Hello world!";
     }
 
-    @RequestMapping("/user")
+//    @RequestMapping("/user")
+    @GetMapping("/user")
     public User userObject(){
-        Random random = new Random();
+        User user;
+        user = new User();
+        user.setId(random.nextInt());
+        user.setName("Akshay Chaudhary");
+        user.setEmail("user@akshay.com");
+
+        return user;
+    }
+
+    /*@GetMapping("/request-param")
+    public String requestParam(@RequestParam String name){
+        return "Your name is : " + name;
+    }*/
+
+    @GetMapping("/path-variable/{id}")
+    public User getUserById(@PathVariable String id) {
+        // Search for the user by ID in the list of sample users
+        for (User user : users) {
+            if (user.getId() == Integer.parseInt(id)) {
+                return user;
+            }
+        }
+        User user;
+        user = new User();
+        user.setId(random.nextInt());
+        user.setName("Akshay Chaudhary");
+        user.setEmail("user@akshay.com");
+
+        return user;// User not found
+    }
+
+    @GetMapping("/request-param")
+    public User getUserByName(@RequestParam String name) {
+        // Search for the user by name in the list of sample users
+        for (User user : users) {
+            if (user.getName().equalsIgnoreCase(name)) {
+                return user;
+            }
+
+        }
         User user;
         user = new User();
         user.setId(random.nextInt());
